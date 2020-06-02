5ca501ffa01f237fb909b3c9fdd5b7a3e0d5f333
Alberto Silva <alberto.silva5456@gmail.com>
2020-06-01 21:57:56 -0300

    RESOLUTION OF THE ERROR WITH THE USER

--- src/Router/Task.js
+++ src/Router/Task.js
@@ -7,7 +7,7 @@ const authMiddleware = require('../middleware/auth')
 router.use(authMiddleware)
 
 router.post('/create/:idUser',taskController.create)
-router.get('/tarefas',taskController.onetasks)
+router.get('/tarefas/:id',taskController.onetasks)
 router.get('/:idUser/tarefas',taskController.alltasks)
 router.delete('/delete/:id',taskController.delete)
 router.put('/editar/:id',taskController.update)
