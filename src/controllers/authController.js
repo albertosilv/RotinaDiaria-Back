08d467a109510c33bf2e02f2cce9b02c2c0a4b42
Alberto Silva <alberto.silva5456@gmail.com>
2020-06-12 19:35:29 -0300

    heroku

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -17,7 +17,7 @@ module.exports = {
         const { email } = req.body
         try {
             if (await User.findOne({ email }))
-                return res.status(400).send({ error: 'User already exists' })
+                return res.send({ error:'Usuário já existe' })
             const user = await User.create(req.body)
             user.password = undefined
             return res.send({
@@ -25,7 +25,7 @@ module.exports = {
                 token: generateToken({ id: user.id })
             });
         } catch (error) {
-            return res.status(400).send({ error: 'Registration failed' });
+            return res.status(400);
         }
     },
     async login(req, res) {
@@ -33,10 +33,10 @@ module.exports = {
             const { email, password } = req.body
             const user = await User.findOne({ email }).select('+password')
             if (!user) {
-                res.status(400).send({ error: 'User not found' })
+                res.status(400).send({ error: 'Usuario não existe' })
             }
             if (!await bscrypt.compare(password, user.password)) {
-                return res.status(400).send({ error: 'Invalid password' })
+                return res.status(400).send({ error: 'Senha invalida' })
             }
             user.password = undefined
             res.send({
@@ -44,7 +44,7 @@ module.exports = {
                 token: generateToken({ id: user.id })
             })
         } catch (err) {
-            return res.status(400).send({ error: 'Login failed' });
+            return res.status(400);
         }
     },
 
@@ -52,7 +52,7 @@ module.exports = {
         try {
             const token = await authentication(req.headers.authorization);
             if (typeof token == 'object') {
-                return res.status(401).send(token)
+                return res.send(token)
             }
             const registro = await Token.create({ token })
             return res.status(201).json({ registro })
@@ -60,7 +60,7 @@ module.exports = {
         }
         catch (error) {
             console.log(error)
-            return res.status(400).send({ error: 'Logout failed' });
+            return res.status(400);
         }
     }
 }
